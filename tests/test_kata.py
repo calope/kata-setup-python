5c1359d296498b3fa6e10d3d85020be963276e17
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-12-16 17:16:50 +0000

    chore: add test with mock

--- tests/test_kata.py
+++ tests/test_kata.py
@@ -1,4 +1,6 @@
-from src.dependency import Dependency
+from unittest.mock import MagicMock, Mock
+
+from src.dependency import Dependency, DependencyInterface
 from src.kata import Kata
 
 
@@ -10,3 +12,13 @@ class TestKata:
         actual = kata.value()
 
         assert actual == 1
+
+    def test_value_should_work_with_mocked_dependency(self):
+        mocked_dependency: DependencyInterface = Mock()
+        mocked_dependency.value = MagicMock(return_value=42)
+        kata = Kata(mocked_dependency)
+
+        actual = kata.value()
+
+        assert actual == 42
+        mocked_dependency.value.assert_called_once()
