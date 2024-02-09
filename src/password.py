ce28eeb861b2c2adfae4dcb99d25c86c4c104526
Carlos Lopez Perez <clopez@fmit-solutions.com>
2024-02-09 18:21:46 +0000

    feat: check at least one _ in password

--- src/password.py
+++ src/password.py
@@ -3,7 +3,9 @@ class Password:
     
     @classmethod
     def is_valid(cls, password: str) -> bool:
+        # return False
         return len(password) >= 8 and \
                any(c.isupper() for c in password) and \
                any(c.islower() for c in password) and \
-               any(c.isdigit() for c in password) 
+               any(c.isdigit() for c in password) and \
+               any(c == "_" for c in password)
--- tests/test_password.py
+++ tests/test_password.py
@@ -1,28 +1,40 @@
 
 from src.password import Password
 
+VALID_PASSWORD = "AABBaa1_"
+
 def test_minimun_eight_length():
     
-    invalid_password = "3"*3
+    invalid_password = VALID_PASSWORD[:3]
     
     assert not Password.is_valid(invalid_password)
+    
+def test_is_valid():
+    
+    assert Password.is_valid(VALID_PASSWORD)
 
 
 def test_at_least_one_uppercase():
     
-    invalid_password = "abcdefgh"
+    invalid_password = VALID_PASSWORD.lower()
     
     assert not Password.is_valid(invalid_password)
     
 def test_at_least_one_lowercase():
     
-    invalid_password = "ABCDEFGH"
+    invalid_password = VALID_PASSWORD.upper()
     
     assert not Password.is_valid(invalid_password)
 
 
 def test_at_least_one_number():
     
-    invalid_password = "AbCDEFGH"
+    invalid_password = VALID_PASSWORD.replace("1", "A")
+    
+    assert not Password.is_valid(invalid_password)
+
+def test_at_least_one_underscore():
+    
+    invalid_password = VALID_PASSWORD.replace("_", "A")
     
     assert not Password.is_valid(invalid_password)
