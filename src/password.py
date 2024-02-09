5b1a1fc6010991f95a554523edece5c77d24b4d1
Carlos Lopez Perez <clopez@fmit-solutions.com>
2024-02-09 17:57:16 +0000

    feat: check upper and lower case

--- src/password.py
+++ src/password.py
@@ -3,4 +3,6 @@ class Password:
     
     @classmethod
     def is_valid(cls, password: str) -> bool:
-        return len(password) >= 8
+        return len(password) >= 8 and \
+               any(c.isupper() for c in password) and \
+               any(c.islower() for c in password)
--- tests/test_password.py
+++ tests/test_password.py
@@ -6,3 +6,17 @@ def test_minimun_eight_length():
     invalid_password = "3"*3
     
     assert not Password.is_valid(invalid_password)
+
+
+def test_at_least_one_uppercase():
+    
+    invalid_password = "abcdefgh"
+    
+    assert not Password.is_valid(invalid_password)
+    
+def test_at_least_one_lowercase():
+    
+    invalid_password = "ABCDEFGH"
+    
+    assert not Password.is_valid(invalid_password)
+
