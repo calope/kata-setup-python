87467cbdee4dc87228843233d984e5f070afa4c5
Carlos Lopez Perez <clopez@fmit-solutions.com>
2024-02-09 18:38:57 +0000

    refactor: better comprehension

--- src/password.py
+++ src/password.py
@@ -2,11 +2,14 @@
 class Password:
     
     MINIMUM_LENGTH = 8
+    AT_LEAST_ONE = (lambda c: c.isupper(),
+                    lambda c: c.islower(),
+                    lambda c: c.isdigit(),
+                    lambda c: c == "_" )
     
     @classmethod
     def is_valid(cls, password: str) -> bool:
+        
         return len(password) >= cls.MINIMUM_LENGTH and \
-               any(c.isupper() for c in password) and \
-               any(c.islower() for c in password) and \
-               any(c.isdigit() for c in password) and \
-               any(c == "_" for c in password)
+               all(any(rule(c) for c in password) 
+                    for rule in cls.AT_LEAST_ONE)
